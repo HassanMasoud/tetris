1291ab0712e994c16d9fb8bdd6a1912fbfb58faf
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-14 22:22:44 +0300

    Initialize javascript

---
+++ app.js
@@ -0,0 +1,5 @@
+const grid = document.querySelector(".grid");
+const width = 10;
+const scoreDisplay = document.querySelector("#score");
+const startBtn = document.querySelector("#start-button");
+let squares = Array.from(document.querySelectorAll(".grid div"));
