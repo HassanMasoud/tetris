2c69c4435490d885bd756e0dc93a5f0be766b7cf
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-14 23:03:41 +0300

    Render a tetromino on the page

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const startBtn = document.querySelector("#start-button");
 let squares = Array.from(document.querySelectorAll(".grid div"));
 
 // Tetrominoes
-const lTetromino = [
+const l1Tetromino = [
   [1, width + 1, width * 2 + 1, 2],
   [width, width + 1, width + 2, width * 2 + 2],
   [1, width + 1, width * 2, width * 2 + 1],
   [width, width * 2, width * 2 + 1, width * 2 + 2],
+];
+
+const l2Tetromino = [
+  [0, 1, width + 1, width * 2 + 1],
+  [width + 2, width * 2, width * 2 + 1, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2 + 2],
+  [width, width + 1, width + 2, width * 2],
 ];
 
 const sTetromino = [
@@ -46,3 +53,24 @@ const iTetromino = [
   [1, width + 1, width * 2 + 1, width * 3 + 1],
   [width, width + 1, width + 2, width + 3],
 ];
+
+const theTetrominoes = [
+  l1Tetromino,
+  l2Tetromino,
+  sTetromino,
+  zTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino,
+];
+
+let currentPositon = 4;
+let current = theTetrominoes[0][0];
+
+function draw() {
+  current.forEach((index) => {
+    squares[currentPositon + index].classList.add("tetromino");
+  });
+}
+
+draw();
