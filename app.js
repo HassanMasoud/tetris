15780ab1213c3ba059b6e56d84434b8da202feea
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-14 22:46:56 +0300

    Create tetrominoes

--- app.js
+++ app.js
@@ -3,3 +3,46 @@ const width = 10;
 const scoreDisplay = document.querySelector("#score");
 const startBtn = document.querySelector("#start-button");
 let squares = Array.from(document.querySelectorAll(".grid div"));
+
+// Tetrominoes
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2, width * 2 + 1],
+  [width, width * 2, width * 2 + 1, width * 2 + 2],
+];
+
+const sTetromino = [
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+];
+
+const zTetromino = [
+  [width, width + 1, width * 2 + 1, width * 2 + 2],
+  [1, width, width + 1, width * 2],
+  [width, width + 1, width * 2 + 1, width * 2 + 2],
+  [1, width, width + 1, width * 2],
+];
+
+const tTetromino = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1],
+];
+
+const oTetromino = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+];
+
+const iTetromino = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+];
