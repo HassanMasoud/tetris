5cb8c58114e9c8e88d71efa558332c954a1c87e1
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-15 13:55:45 +0300

    Add rotate functionality

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ function control(e) {
   if (e.keyCode == 37) {
     moveLeft();
   } else if (e.keyCode == 38) {
-    //rotate
+    rotate();
   } else if (e.keyCode == 39) {
     moveRight();
   } else if (e.keyCode == 40) {
@@ -156,6 +156,17 @@ function moveRight() {
   ) {
     currentPosition -= 1;
   }
+
+  draw();
+}
+
+function rotate() {
+  undraw();
+  currentRotation++;
+  if (currentRotation == current.length) {
+    currentRotation = 0;
+  }
+  current = theTetrominoes[random][currentRotation];
 
   draw();
 }
