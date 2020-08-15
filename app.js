d9786777bf55293f4f90087c3a127b73c3be7dbd
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-15 13:48:20 +0300

    Add move right functionality

--- app.js
+++ app.js
@@ -87,6 +87,12 @@ timerId = setInterval(moveDown, 1000);
 function control(e) {
   if (e.keyCode == 37) {
     moveLeft();
+  } else if (e.keyCode == 38) {
+    //rotate
+  } else if (e.keyCode == 39) {
+    moveRight();
+  } else if (e.keyCode == 40) {
+    moveDown();
   }
 }
 
@@ -130,6 +136,25 @@ function moveLeft() {
     )
   ) {
     currentPosition += 1;
+  }
+
+  draw();
+}
+
+function moveRight() {
+  undraw();
+  const isAtRightEdge = current.some(
+    (index) => (currentPosition + index) % width === width - 1
+  );
+
+  if (!isAtRightEdge) currentPosition += 1;
+
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains("taken")
+    )
+  ) {
+    currentPosition -= 1;
   }
 
   draw();
